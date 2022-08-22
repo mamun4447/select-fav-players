c58ea0cbe669db9eeeb520adea843701565b422b
abdullah <abdullah15-12557@diu.edu.bd>
2022-08-22 11:11:15 +0600

    Heading change

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const selectBtn = document.getElementsByClassName('selectBtn');
 for (let Select of selectBtn) {
     Select.addEventListener('click', function(event) {
         const playerName = event.target.parentNode.parentNode.firstChild.nextSibling.innerText;
-        event.target.style.disabled = true;
+        // event.target.style.disabled = true;
         // console.log(heading);
         const fivePlayers = document.getElementById("playerName");
         const ol = document.createElement('ol');
